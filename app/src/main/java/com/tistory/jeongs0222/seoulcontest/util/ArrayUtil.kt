a1c65461eec115016149c274ce212ee44a250b7a
정현성 <jin55789@naver.com>
2018-08-29 17:45:40 +0900

    Font 파일 적용

--- app/src/main/java/com/tistory/jeongs0222/seoulcontest/util/ArrayUtil.kt
+++ app/src/main/java/com/tistory/jeongs0222/seoulcontest/util/ArrayUtil.kt
@@ -4,7 +4,7 @@ package com.tistory.jeongs0222.seoulcontest.util
 object ArrayUtil {
 
     val paintColorList = arrayOf("#FFFFFF", "#BDBDBD", "#A6A6A6", "#000000", "#f98da5", "#FF7F00", "#FF0000", "#FFFF00", "#3395ff", "#2278d4", "#e4b5fc", "#a854d4")
-    val fontList = arrayOf("나눔스퀘어라운드", "나눔스퀘어", "야체", "몬소리체", "빙그레체")
-    val fontsList = arrayOf("fonts/NanumSquareRoundOTFR.otf", "fonts/NanumSquareOTFRegular.otf")
+    val fontList = arrayOf("고양체", "나눔스퀘어라운드", "나눔스퀘어", "주아체", "한나는11살체", "고도라운드체", "고도마음체", "야놀자체", "미생체", "신비는7살체", "꽃길체", "개미똥구멍체")
+    val fontsList = arrayOf("fonts/goyang.otf", "fonts/NanumSquareRoundOTFR.otf", "fonts/NanumSquareOTFRegular.otf", "fonts/bmjua.otf", "fonts/bmhanna_11yrs.otf", "fonts/godoRoundedR.ttf", "fonts/godoMaum.ttf", "fonts/yanoljaR.otf", "fonts/SDMiSaeng.ttf", "fonts/SSShinb7.ttf", "fonts/SangSangFlowerRoad.otf", "fonts/SangSangAnt.otf")
     val stampList = arrayOf(1, 2, 3, 4, 5, 6, 7, 8)
 }

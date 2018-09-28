ae85a797cd843bd63b3dcfc74b6e18b95fb992a7
정현성 <jin55789@naver.com>
2018-09-28 15:22:48 +0900

    PhotoshopFontAdapter  - 폰트 변경 Adapter 디자인 변경

--- app/src/main/java/com/tistory/jeongs0222/seoulcontest/ui/photoshop/PhotoshopFontAdapter.kt
+++ app/src/main/java/com/tistory/jeongs0222/seoulcontest/ui/photoshop/PhotoshopFontAdapter.kt
@@ -1,14 +1,13 @@
 package com.tistory.jeongs0222.seoulcontest.ui.photoshop
 
 import android.content.Context
+import android.graphics.Typeface
 import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import com.bumptech.glide.Glide
 import com.tistory.jeongs0222.seoulcontest.R
 import com.tistory.jeongs0222.seoulcontest.util.ArrayUtil
 
@@ -27,12 +26,9 @@ class PhotoshopFontAdapter(internal val context: Context, val callback: (Int) ->
 
         val items = item[position]
 
-        Glide.with(context)
-                .asBitmap()
-                .load(R.drawable.temporary)
-                .into(holder.font_imageView)
+        holder.font_temporary_textView.typeface = Typeface.createFromAsset(context.assets, ArrayUtil.fontsList[position])
 
-        holder.font_textView.text = item[position]
+        holder.font_textView.text = items
 
         holder.font_entire_constraintLayout.setOnClickListener {
             callback(position)
@@ -44,12 +40,12 @@ class PhotoshopFontAdapter(internal val context: Context, val callback: (Int) ->
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val font_entire_constraintLayout: ConstraintLayout
-        val font_imageView: ImageView
+        val font_temporary_textView: TextView
         val font_textView: TextView
 
         init {
             font_entire_constraintLayout = itemView.findViewById(R.id.font_entire_constraintLayout)
-            font_imageView = itemView.findViewById(R.id.font_imageView)
+            font_temporary_textView = itemView.findViewById(R.id.font_temporary_textView)
             font_textView = itemView.findViewById(R.id.font_textView)
         }
     }
